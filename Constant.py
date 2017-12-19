93549ee1cfa1147b897a02c52e70e17b59a11118
Wasin Watthanasrisong <wasin.w@taskworld.com>
2017-12-20 00:43:16 +0700

    config

---
+++ Constant.py
@@ -0,0 +1,2 @@
+Token = 'TggCqD9x2zDV6JxA2lmx5toFM4MfLy6l5ocDsc9OfphBAIQxrXj6ul7ulst06le6afdnqu0l/n8gdOpbLsj0l2h0n7Qc8pKxcCDIipOW+hsFH39t6ykcMpGXtRrpx+ghifLmLDczIUSP+sZgf2yCwgdB04t89/1O/w1cDnyilFU='
+MONGO_URI = 'mongodb://wasin:notesource@ds159866.mlab.com:59866/werewolf-bot'
